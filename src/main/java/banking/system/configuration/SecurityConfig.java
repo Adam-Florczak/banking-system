aa9b4a9e404d2b143c0c5fbfc5bb32149a3c14d2
Adam Florczak <adam.florczak93@gmail.com>
2018-06-29 19:16:50 +0200

    Registration form - communicating with front

--- src/main/java/banking/system/configuration/SecurityConfig.java
+++ src/main/java/banking/system/configuration/SecurityConfig.java
@@ -4,18 +4,53 @@ import banking.system.client.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Lazy
     @Autowired
     private ClientService clientService;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/registration.html").permitAll()
+                .antMatchers("/registration").permitAll()
+                .anyRequest().permitAll();
+
+/*        http
+                .formLogin()
+                .loginPage("/registration")
+                .failureHandler(
+                        (req, resp, e) -> resp.sendError(HttpStatus.BAD_REQUEST.value(), "Username or password invalid"))
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/security-sandbox/welcome").permitAll();*/
+        http
+                .logout()
+                .logoutUrl("/users/logout")
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl("/hello")
+                .permitAll();
+
+    }
+
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
