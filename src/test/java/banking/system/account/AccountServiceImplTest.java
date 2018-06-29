9798dbefdfbadd7c211302199b230d9ab5b31d85
pirat21wieka <pirat21wieka@gmail.com>
2018-06-29 19:31:54 +0200

    AccountServiceImplTest

---
+++ src/test/java/banking/system/account/AccountServiceImplTest.java
@@ -0,0 +1,17 @@
+package banking.system.account;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findById() {
+    }
+}
