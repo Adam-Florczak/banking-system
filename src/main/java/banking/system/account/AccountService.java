67280758c3eb29e65627637f9f512e0bf7cdadbf
Maciej Król <37816451+maciejkrolpl@users.noreply.github.com>
2018-06-29 18:13:37 +0200

    Features/account services (#12)
    * Created repo,DTO; service in progress...
    
    * Creating account in progress...
    
    * Creating account done
    
    * Validation during creating account

---
+++ src/main/java/banking/system/account/AccountService.java
@@ -0,0 +1,18 @@
+package banking.system.account;
+
+import java.util.Set;
+
+public interface AccountService {
+
+    Account findById(Long id);
+
+    Set<Account> findAll();
+
+    void deleteOneById(Long id);
+
+    Account createAccount(AccountCreateDTO accountCreateDTO);
+
+    Boolean isAccountExists(String accountNumber);
+
+    String generateAccountNumber();
+}
