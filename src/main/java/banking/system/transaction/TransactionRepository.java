7b2efc8a6d64cf45a13b20a3eab15dcf9d37f6f1
Damian Nycz <damianycz@gmail.com>
2018-07-02 19:34:34 +0200

    Add transaction service and repository

---
+++ src/main/java/banking/system/transaction/TransactionRepository.java
@@ -0,0 +1,8 @@
+package banking.system.transaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface TransactionRepository extends JpaRepository <Transaction, Long> {
+    Optional<Transaction> findById(Long id);
+}
