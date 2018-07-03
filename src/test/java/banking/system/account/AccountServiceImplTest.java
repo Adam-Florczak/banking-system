e3b59dac40c2e1ffe4ff38921fdbbc7e3ca504cc
pirat21wieka <pirat21wieka@gmail.com>
2018-07-03 18:02:32 +0200

    Added next AccountService test

--- src/test/java/banking/system/account/AccountServiceImplTest.java
+++ src/test/java/banking/system/account/AccountServiceImplTest.java
@@ -4,39 +4,30 @@ import banking.system.client.Client;
 import banking.system.client.ClientRepository;
 import banking.system.common.Currency;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
-import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class AccountServiceImplTest {
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
     @Autowired
     AccountRepository accountRepository;
-
     @Autowired
     ClientRepository clientRepository;
-
-
-    private AccountCreateDTO prepareAccountDto(AccountServiceImpl accountService) {
-
-        AccountCreateDTO accountCreateDTO = new AccountCreateDTO();
-        accountCreateDTO.setNumber(accountService.generateAccountNumber());
-        accountCreateDTO.setOwner(clientRepository.save(prepareClient()));
-        accountCreateDTO.setCurrency(Currency.PLN);
-        accountCreateDTO.setInterest(BigDecimal.ZERO);
-        accountCreateDTO.setProvision(BigDecimal.ZERO);
-        accountCreateDTO.setAccountType(AccountType.PERSONAL);
-        return accountCreateDTO;
-    }
 
     private Client prepareClient() {
         Client client = new Client();
@@ -46,19 +37,173 @@ public class AccountServiceImplTest {
     }
 
     @Test
-    public void givenNewAccountEntity_WhenSavedAndRetrievingFromDb_ThenOk() {
+    public void givenCorrectPersonalAccount_WhenSavedAndRetrievingFromDb_ThenNotNull() {
 
+        //given
         AccountServiceImpl accountService = new AccountServiceImpl(accountRepository);
+        AccountCreateDTO accountCreateDTO = new AccountCreateDTO();
+        accountCreateDTO.setOwner(clientRepository.save(prepareClient()));
+        accountCreateDTO.setCurrency(Currency.PLN);
+        accountCreateDTO.setInterest(BigDecimal.ZERO);
+        accountCreateDTO.setProvision(BigDecimal.ZERO);
+        accountCreateDTO.setAccountType(AccountType.PERSONAL);
 
-        AccountCreateDTO accountCreateDTO = prepareAccountDto(accountService);
+        //when
+        Account saved = accountService.createAccount(accountCreateDTO);
+        Account account = accountService.findById(saved.getId());
 
-        accountService.createAccount(accountCreateDTO);
-
-        Account account = accountService.findById(1L);
-
+        //then
         Assert.assertNotNull(account);
+    }
+
+
+
+
+    @Test
+    public void givenCorrectAccount_WhenSavedDeletedAndRetrievingFromDb_ThenThrowsException() {
+
+        //given
+        AccountServiceImpl accountService = new AccountServiceImpl(accountRepository);
+        AccountCreateDTO accountCreateDTO = new AccountCreateDTO();
+        
+        accountCreateDTO.setOwner(clientRepository.save(prepareClient()));
+        accountCreateDTO.setCurrency(Currency.PLN);
+        accountCreateDTO.setInterest(BigDecimal.ZERO);
+        accountCreateDTO.setProvision(BigDecimal.ZERO);
+        accountCreateDTO.setAccountType(AccountType.PERSONAL);
+
+        //when
+        Account saved = accountService.createAccount(accountCreateDTO);
+        Long id = saved.getId();
+        accountService.deleteOneById(id);
+
+        //then
+        expectedException.expect(RuntimeException.class);
+        accountService.findById(id);
+    }
+
+    @Test
+    public void givenPersonalAccount_WhenForeignCurrencySet_ThenThrowsException() {
+
+        //given
+        AccountServiceImpl accountService = new AccountServiceImpl(accountRepository);
+        AccountCreateDTO accountCreateDTO = new AccountCreateDTO();
+        
+        accountCreateDTO.setOwner(clientRepository.save(prepareClient()));
+        accountCreateDTO.setInterest(BigDecimal.ZERO);
+        accountCreateDTO.setProvision(BigDecimal.ZERO);
+        accountCreateDTO.setAccountType(AccountType.PERSONAL);
+
+        //when
+        accountCreateDTO.setCurrency(Currency.CHF);
+
+        //then
+        expectedException.expect(RuntimeException.class);
+        accountService.createAccount(accountCreateDTO);
+    }
+
+    @Test
+    public void givenPersonalAccount_WhenProvisionBiggerThanZeroSet_ThenThrowsException() {
+
+        //given
+        AccountServiceImpl accountService = new AccountServiceImpl(accountRepository);
+        AccountCreateDTO accountCreateDTO = new AccountCreateDTO();
+        
+        accountCreateDTO.setOwner(clientRepository.save(prepareClient()));
+        accountCreateDTO.setInterest(BigDecimal.ZERO);
+        accountCreateDTO.setAccountType(AccountType.PERSONAL);
+        accountCreateDTO.setCurrency(Currency.PLN);
+
+        //when
+        accountCreateDTO.setProvision(BigDecimal.ONE);
+
+        //then
+        expectedException.expect(RuntimeException.class);
+        accountService.createAccount(accountCreateDTO);
+    }
+
+    @Test
+    public void givenPersonalAccount_WhenInterestBiggerThanZeroSet_ThenThrowsException() {
+
+        //given
+        AccountServiceImpl accountService = new AccountServiceImpl(accountRepository);
+        AccountCreateDTO accountCreateDTO = new AccountCreateDTO();
+        
+        accountCreateDTO.setOwner(clientRepository.save(prepareClient()));
+        accountCreateDTO.setProvision(BigDecimal.ZERO);
+        accountCreateDTO.setAccountType(AccountType.PERSONAL);
+        accountCreateDTO.setCurrency(Currency.PLN);
+
+        //when
+        accountCreateDTO.setInterest(BigDecimal.ONE);
+
+        //then
+        expectedException.expect(RuntimeException.class);
+        accountService.createAccount(accountCreateDTO);
+    }
+
+    @Test
+    public void givenCorrectForeignCurrencyAccount_WhenSavedAndRetrievingFromDb_ThenNotNull() {
+
+        //given
+        AccountServiceImpl accountService = new AccountServiceImpl(accountRepository);
+        AccountCreateDTO accountCreateDTO = new AccountCreateDTO();
+        
+        accountCreateDTO.setOwner(clientRepository.save(prepareClient()));
+        accountCreateDTO.setCurrency(Currency.CHF);
+        accountCreateDTO.setInterest(BigDecimal.ZERO);
+        accountCreateDTO.setProvision(BigDecimal.ONE);
+        accountCreateDTO.setAccountType(AccountType.FOREIGNCURRENCY);
+
+        //when
+        Account saved = accountService.createAccount(accountCreateDTO);
+        Account account = accountService.findById(saved.getId());
+
+        //then
+        Assert.assertNotNull(account);
+    }
+
+
+
+    @Test
+    public void givenCorrectSavingsAccount_WhenSavedAndRetrievingFromDb_ThenNotNull() {
+
+        //given
+        AccountServiceImpl accountService = new AccountServiceImpl(accountRepository);
+        AccountCreateDTO accountCreateDTO = new AccountCreateDTO();
+        
+        accountCreateDTO.setOwner(clientRepository.save(prepareClient()));
+        accountCreateDTO.setCurrency(Currency.PLN);
+        accountCreateDTO.setInterest(BigDecimal.ONE);
+        accountCreateDTO.setProvision(BigDecimal.ZERO);
+        accountCreateDTO.setAccountType(AccountType.SAVINGS);
+
+        //when
+        Account saved = accountService.createAccount(accountCreateDTO);
+        Account account = accountService.findById(saved.getId());
+
+        //then
+        Assert.assertNotNull(account);
+    }
+
+    @Test
+    public void givenAccountWithExistingNumber_WhenCheckingIfNumberAlreadyExists_ThenReturnsOk() {
+        AccountServiceImpl accountService = new AccountServiceImpl(accountRepository);
+        String number = "PL12345678";
+        Account account = new Account();
+        account.setType(AccountType.PERSONAL);
+        account.setCurrency(Currency.PLN);
+        account.setOwner(clientRepository.save(prepareClient()));
+        account.setNumber(number);
+        account.setBalance(BigDecimal.ZERO);
+        accountRepository.save(account);
+
+        Boolean isExisting = accountService.isAccountExists(number);
+
+        Assert.assertTrue(isExisting);
 
     }
+
 
 
 }
