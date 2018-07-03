af0b75dc2565944bdf9cbbb97139c1f5712150bd
Damian Nycz <damianycz@gmail.com>
2018-07-03 20:15:45 +0200

    Change TransactionDTO
    Add finding Account by number
    
    Miss currency exchange transaction

--- src/main/java/banking/system/transaction/TransactionServiceImpl.java
+++ src/main/java/banking/system/transaction/TransactionServiceImpl.java
@@ -1,30 +1,38 @@
 package banking.system.transaction;
 
+import banking.system.account.Account;
+import banking.system.account.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.PostPersist;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class TransactionServiceImpl implements TransactionService{
+public class TransactionServiceImpl implements TransactionService {
 
-    TransactionRepository transactionRepository;
+    private TransactionRepository transactionRepository;
+    private AccountRepository accountRepository;
 
     @Autowired
-    public TransactionServiceImpl(TransactionRepository transactionRepository){
-        this.transactionRepository=transactionRepository;
+    public TransactionServiceImpl(TransactionRepository transactionRepository, AccountRepository accountRepository) {
+        this.transactionRepository = transactionRepository;
+        this.accountRepository = accountRepository;
     }
 
     @Override
     public Transaction findById(Long id) {
-        Optional<Transaction> optionalTransaction= transactionRepository.findById(id);
-        if(optionalTransaction.isPresent()){
+        Optional<Transaction> optionalTransaction = transactionRepository.findById(id);
+        if (optionalTransaction.isPresent()) {
             return optionalTransaction.get();
-        }
-        else{
+        } else {
             throw new RuntimeException("No transaction found");
         }
     }
@@ -42,25 +50,77 @@ public class TransactionServiceImpl implements TransactionService{
     }
 
     @Override
-    public Set<Transaction> findAllToAccount(Long id){
+    public Set<Transaction> findAllToAccount(Long id) {
         return findAll().stream()
                 .filter(transaction -> transaction.getTo().getId().equals(id))
                 .collect(Collectors.toSet());
     }
 
-    public Set<Transaction> findAllOnAccount(Long id){
-
-        Set<Transaction> result=findAllFromAccount(id);
+    public Set<Transaction> findAllOnAccount(Long id) {
+        Set<Transaction> result = findAllFromAccount(id);
         result.addAll(findAllToAccount(id));
         return result;
 
     }
 
-
-
+    @Transactional
     @Override
-    public Transaction createTransaction(Transaction transaction) {
-        return transactionRepository.save(transaction);
+    public Transaction createTransaction(TransactionDTO transactionDTO) {
+
+//        Long fromID = transactionDTO.getFrom().getId();
+//        Long toID = transactionDTO.getTo().getId();
+
+        String fromNumber = transactionDTO.getFromNumber();
+        String toNumber = transactionDTO.getToNumber();
+
+
+        if(transactionDTO.getDueDate().isBefore(LocalDateTime.now())){
+            throw new RuntimeException("No option for create past transaction");
+        }
+        if(fromNumber.equals(toNumber)){
+            throw new RuntimeException("No self-transaction allow");
+        }
+
+        Account from = accountRepository.findOneByNumber(fromNumber).orElseThrow(RuntimeException::new);
+        Account to = accountRepository.findOneByNumber(toNumber).orElseThrow(RuntimeException::new);
+
+        if(transactionDTO.getAmount().compareTo(from.getBalance())>0){
+            throw new RuntimeException("You're too poor exception");
+        }
+
+        if(transactionDTO.getAmount().compareTo(BigDecimal.ZERO)<0){
+            throw new RuntimeException("Negative amount exception");
+        }
+
+        if(transactionDTO.getTitle().equals("")){
+            throw new RuntimeException("No title transaction exception");
+        }
+
+        //Todo Co z walutami?
+        if(!from.getCurrency().equals(transactionDTO.getCurrency())){
+            //todo prowizja1
+
+        }
+        if(!transactionDTO.getCurrency().equals(to.getCurrency())){
+            //todo proizja2
+
+        }
+
+        Transaction transaction = new Transaction();
+        transaction.setFrom(from);
+        transaction.setTo(to);
+        transaction.setAmount(transactionDTO.getAmount());
+        transaction.setTitle(transactionDTO.getTitle());
+        transaction = transactionRepository.save(transaction);
+
+        from = accountRepository.findOneByNumber(fromNumber).orElseThrow(RuntimeException::new);
+        to = accountRepository.findOneByNumber(toNumber).orElseThrow(RuntimeException::new);
+        from.setBalance(from.getBalance().subtract(transaction.getAmount()));
+        to.setBalance(from.getBalance().add(transaction.getAmount()));
+        accountRepository.save(from);
+        accountRepository.save(to);
+
+        return transaction;
     }
 
     @Override
