5b13eddbfc119c09b2621a1451c9df88f0da79de
Maciej Król <37816451+maciejkrolpl@users.noreply.github.com>
2018-07-04 17:23:50 +0200

    Features/exchange rate synch (#32)
    * AccountServiceImplTest
    
    * Test if context loads
    
    * Create account test
    
    * Repository findById test
    
    * Service test passing
    
    * Added next AccountService test
    
    * Created package and classes
    
    * Working method getting latest rates from api
    
    * AccountServiceImplTest
    
    * Test if context loads
    
    * Create account test
    
    * Repository findById test
    
    * Service test passing
    
    * Added next AccountService test
    
    * Created package and classes
    
    * Working method getting latest rates from api
    
    * Working method getting latest rates from api

--- src/main/java/banking/system/exchangerate/ExchangeRateServiceImpl.java
+++ src/main/java/banking/system/exchangerate/ExchangeRateServiceImpl.java
@@ -1,47 +1,50 @@
-//package banking.system.exchangerate;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.io.IOException;
-//import java.math.BigDecimal;
-//
-//@Service
-//public class ExchangeRateServiceImpl implements ExchangeRateService {
-//
-//    private final BigDecimal RATE = new BigDecimal("0.05");
-//    private ExchangeRateRepository repository;
-//
-//    @Autowired
-//    public ExchangeRateServiceImpl(ExchangeRateRepository repository) {
-//        this.repository = repository;
-//    }
-//
-//    @Override
-//    public ExchangeRate findLast() {
-//        return repository.findOneByCreatedAtOrderByCreatedAtDesc();
-//    }
-//
-//    @Override
-//    public ExchangeRate createCurrent() {
-//        CurrencyApi api = new CurrencyApi();
-//        ExchangeRate rate = new ExchangeRate();
-//
-//        try {
-//            rate.setEur(api.getRatio("EUR"));
-//            rate.setGbp(api.getRatio("GBP"));
-//            rate.setUsd(api.getRatio("USD"));
-//            rate.setChf(api.getRatio("CHF"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//        return repository.save(rate);
-//
-//
-//    }
-//
-//
-//
-//}
+
+package banking.system.exchangerate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+@Service
+public class ExchangeRateServiceImpl implements ExchangeRateService {
+
+    private ExchangeRateRepository repository;
+
+    @Autowired
+    public ExchangeRateServiceImpl(ExchangeRateRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public ExchangeRate findLast() {
+//        return repository.findOneOrderByCreatedAtDesc();
+    return null;
+    }
+
+
+    @Override
+    public ExchangeRate createCurrent() {
+        CurrencyApi api = new CurrencyApi();
+        ExchangeRate rate = new ExchangeRate();
+
+        try {
+            rate.setEur(api.getRatio("EUR"));
+            rate.setGbp(api.getRatio("GBP"));
+            rate.setUsd(api.getRatio("USD"));
+            rate.setChf(api.getRatio("CHF"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return repository.save(rate);
+
+
+    }
+
+
+
+}
+
