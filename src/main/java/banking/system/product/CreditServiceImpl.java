4120a2f3c07750d0679ca7d19890a56fd0eb79a9
Damian Nycz <damianycz@gmail.com>
2018-07-05 19:27:05 +0200

    Add Credit service

--- src/main/java/banking/system/product/CreditServiceImpl.java
+++ src/main/java/banking/system/product/CreditServiceImpl.java
@@ -3,9 +3,11 @@ import banking.system.account.Account;
 import banking.system.account.AccountRepository;
 import banking.system.account.AccountType;
 import banking.system.transaction.Transaction;
+import banking.system.transaction.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.security.auth.login.AccountNotFoundException;
 import java.math.BigDecimal;
@@ -19,11 +21,13 @@ public class CreditServiceImpl implements CreditService{
     private final String bankAccountNumberPrefix="PL99769997";
     private CreditRepository creditRepository;
     private AccountRepository accountRepository;
+    private TransactionRepository transactionRepository;
 
     @Autowired
-    public CreditServiceImpl(CreditRepository creditRepository, AccountRepository accountRepository) {
+    public CreditServiceImpl(CreditRepository creditRepository, AccountRepository accountRepository, TransactionRepository transactionRepository) {
         this.creditRepository = creditRepository;
         this.accountRepository=accountRepository;
+        this.transactionRepository=transactionRepository;
     }
 
     @Override
@@ -53,6 +57,7 @@ public class CreditServiceImpl implements CreditService{
         }
     }
 
+    @Transactional
     @Override
     public Credit createTransaction(CreditDTO creditDTO) {
         Credit credit=new Credit();
@@ -64,6 +69,7 @@ public class CreditServiceImpl implements CreditService{
         else{
             throw new RuntimeException("No account found");
         }
+
         credit.setAccount(account);
         credit.setAmount(creditDTO.getAmount());
         credit.setCurrency(creditDTO.getCurrency());
@@ -74,21 +80,32 @@ public class CreditServiceImpl implements CreditService{
         BigDecimal installmentAmount=credit.getAmount().multiply(credit.getInterest())
                 .divide(new BigDecimal(String.valueOf(credit.getInstallmentsQuantity())),BigDecimal.ROUND_DOWN);
         LocalDateTime paymentDate=LocalDateTime.now();
+
+        Account bankAccount;
+        Optional<Account> optionalBankAccount=accountRepository.findOneByNumber(bankAccountNumberPrefix+credit.getCurrency().name());
+        if(optionalBankAccount.isPresent()){
+            bankAccount=optionalBankAccount.get();
+
+        }
+        else{
+            throw new RuntimeException("No correct bank account available");
+        }
+
         for(int i=0;i<credit.getInstallmentsQuantity();i++){
             paymentDate=paymentDate.plusMonths(1L);
-            Account bankAccount;
-            Optional<Account> optionalBankAccount=accountRepository.findOneByNumber(bankAccountNumberPrefix+credit.getCurrency().name());
-            if(optionalBankAccount.isPresent()){
-                bankAccount=optionalBankAccount.get();
-            }
-            else{
-                throw new RuntimeException("No correct bank account available");
-            }
+
             instalmentsSet.add(new Transaction(credit.getAccount(),bankAccount,paymentDate,credit.getCurrency(),installmentAmount,
                     (i+1)+" from "+credit.getInstallmentsQuantity()+" loan installment"));
         }
         credit.setInstallments(instalmentsSet);
-
+        Transaction grant=new Transaction();
+        grant.setTitle("Grant from credit "+credit.getUuid());
+        grant.setFrom(bankAccount);
+        grant.setTo(account);
+        grant.setCurrency(credit.getCurrency());
+        grant.setAmount(credit.getAmount());
+        grant.setDueDate(LocalDateTime.now());
+        transactionRepository.save(grant);
         return creditRepository.save(credit);
 
     }
