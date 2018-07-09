910b82dd7aaf8c379ceef60c85d20894d8365a0a
Kasia <astarte2525@interia.pl>
2018-07-09 20:57:17 +0200

    fix

--- src/main/java/banking/system/transaction/TransactionRepository.java
+++ src/main/java/banking/system/transaction/TransactionRepository.java
@@ -1,8 +1,11 @@
 package banking.system.transaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface TransactionRepository extends JpaRepository <Transaction, Long> {
     Optional<Transaction> findById(Long id);
 }
