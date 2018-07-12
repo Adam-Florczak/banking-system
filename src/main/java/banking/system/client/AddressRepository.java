02c4acfca01cf0a58d3b933f7cc3a53f4e4a86a3
pirat21wieka <pirat21wieka@gmail.com>
2018-07-12 18:15:25 +0200

    Test of method saving new address to db

--- src/main/java/banking/system/client/AddressRepository.java
+++ src/main/java/banking/system/client/AddressRepository.java
@@ -1,9 +1,11 @@
 package banking.system.client;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AddressRepository extends JpaRepository <Address, Long> {
     Optional<Address> findById(Long id);
 }
